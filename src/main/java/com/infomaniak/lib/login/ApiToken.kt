28b519e1054a1b9bb543dc37c8da2b18d01e3d01
Kilian Périsset <kilian.perisset@infomaniak.com>
2020-06-26 18:28:26 +0200

    Replaced ApiToken by data class + Implemented parcelable

--- src/main/java/com/infomaniak/lib/login/ApiToken.kt
+++ src/main/java/com/infomaniak/lib/login/ApiToken.kt
@@ -1,12 +1,15 @@
 package com.infomaniak.lib.login
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
-class ApiToken(
+@Parcelize
+data class ApiToken(
     @SerializedName("access_token") val accessToken: String? = null,
     @SerializedName("refresh_token") val refreshToken: String? = null,
     @SerializedName("token_type") val tokenType: String? = null,
     @SerializedName("expires_in") val expiresIn: Int = 0,
     @SerializedName("user_id") val userId: Int = 0,
     @SerializedName("scope") val scope: String? = null
-)
+) : Parcelable
