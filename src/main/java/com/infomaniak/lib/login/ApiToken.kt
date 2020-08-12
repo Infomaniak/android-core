54b42feb5c541ac04e274548d484a78c66bfc606
Joris Bodin <joris.bodin@infomaniak.com>
2020-08-12 15:35:24 +0200

    Improve ApiToken
    Signed-off-by: Joris Bodin <joris.bodin@infomaniak.com>

--- src/main/java/com/infomaniak/lib/login/ApiToken.kt
+++ src/main/java/com/infomaniak/lib/login/ApiToken.kt
@@ -6,11 +6,11 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class ApiToken(
-    @SerializedName("access_token") val accessToken: String? = null,
-    @SerializedName("refresh_token") val refreshToken: String? = null,
-    @SerializedName("token_type") val tokenType: String? = null,
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("refresh_token") val refreshToken: String,
+    @SerializedName("token_type") val tokenType: String,
     @SerializedName("expires_in") val expiresIn: Int = 7200,
-    @SerializedName("user_id") val userId: Int = 0,
+    @SerializedName("user_id") val userId: Int,
     @SerializedName("scope") val scope: String? = null,
     var expiresAt: Long?
 ) : Parcelable
