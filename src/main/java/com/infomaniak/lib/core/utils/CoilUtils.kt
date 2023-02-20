23947a5499c29e04b36c93e8f19a79826d6ce9ae
Joris Bodin <joris.bodin@infomaniak.com>
2023-02-20 17:53:07 +0100

    Add coilUtils

---
+++ src/main/java/com/infomaniak/lib/core/utils/CoilUtils.kt
@@ -0,0 +1,90 @@
+/*
+ * Infomaniak kDrive - Android
+ * Copyright (C) 2023 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.lib.core.utils
+
+import android.content.Context
+import android.os.Build
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import com.infomaniak.lib.core.BuildConfig
+import com.infomaniak.lib.core.auth.TokenAuthenticator
+import com.infomaniak.lib.core.auth.TokenInterceptor
+import com.infomaniak.lib.core.auth.TokenInterceptorListener
+import com.infomaniak.lib.core.networking.HttpUtils
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+
+object CoilUtils {
+
+    private val COIL_CACHE_DIR = "coil_cache"
+
+    private var simpleImageLoader: ImageLoader? = null
+
+    @JvmStatic
+    fun simpleImageLoader(context: Context): ImageLoader {
+        return simpleImageLoader ?: synchronized(this) {
+            simpleImageLoader?.let { return it }
+            newImageLoader(context)
+        }
+    }
+
+    inline val Context.simpleImageLoader: ImageLoader
+        get() = simpleImageLoader(this)
+
+    fun newImageLoader(
+        context: Context,
+        tokenInterceptorListener: TokenInterceptorListener? = null,
+        gifPreview: Boolean = false
+    ): ImageLoader {
+        return ImageLoader.Builder(context)
+            .crossfade(true)
+            .components {
+                if (gifPreview) {
+                    add(if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) ImageDecoderDecoder.Factory() else GifDecoder.Factory())
+                }
+            }
+            .okHttpClient {
+                OkHttpClient.Builder().apply {
+                    tokenInterceptorListener?.let {
+                        addInterceptor(Interceptor { chain ->
+                            chain.request().newBuilder()
+                                .headers(HttpUtils.getHeaders())
+                                .build()
+                                .let(chain::proceed)
+                        })
+                        addInterceptor(TokenInterceptor(it))
+                        authenticator(TokenAuthenticator(it))
+                    }
+                    if (BuildConfig.DEBUG) {
+                        addNetworkInterceptor(StethoInterceptor())
+                    }
+                }.build()
+            }
+            .memoryCache {
+                MemoryCache.Builder(context).build()
+            }
+            .diskCache {
+                DiskCache.Builder().directory(context.cacheDir.resolve(COIL_CACHE_DIR)).build()
+            }
+            .build()
+    }
+}
