9cbb128e8dca18dc9a6522ffc17fb04b580fafad
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2023-03-09 10:37:44 +0100

    refactor: WebViewLoginActivity

--- src/main/java/com/infomaniak/lib/login/WebViewLoginActivity.kt
+++ src/main/java/com/infomaniak/lib/login/WebViewLoginActivity.kt
@@ -1,19 +1,11 @@
 package com.infomaniak.lib.login
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
-import android.net.http.SslError
-import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.webkit.*
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
 import com.infomaniak.lib.login.InfomaniakLogin.Companion.LOGIN_URL_TAG
 import com.infomaniak.lib.login.InfomaniakLogin.Companion.REMOVE_COOKIES_TAG
 import com.infomaniak.lib.login.databinding.ActivityWebViewLoginBinding
@@ -36,7 +28,7 @@ class WebViewLoginActivity : AppCompatActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_web_view_login)
+        setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
         if (removeCookies) {
@@ -46,8 +38,12 @@ class WebViewLoginActivity : AppCompatActivity() {
 
         binding.webview.apply {
             settings.javaScriptEnabled = true
-            webViewClient = LoginWebViewClient()
-            webChromeClient = ProgressWebChromeClient()
+            webViewClient = LoginWebViewClient(
+                activity = this@WebViewLoginActivity,
+                progressBar = binding.progressBar,
+                appUID = appUID
+            )
+            webChromeClient = ProgressWebChromeClient(binding.progressBar)
             loadUrl(loginUrl)
         }
     }
@@ -65,108 +61,6 @@ class WebViewLoginActivity : AppCompatActivity() {
             }
 
             else -> false
-        }
-    }
-
-    private fun isValidUrl(url: String?): Boolean {
-        if (url == null) return false
-        if (onAuthResponse(Uri.parse(url))) return false
-        return url.contains("login.infomaniak.com")
-                || url.contains("oauth2redirect")
-                || url.contains("gstatic.com")
-                || url.contains("google.com/recaptcha")
-    }
-
-    private fun onAuthResponse(uri: Uri?): Boolean {
-        return if (uri?.scheme == appUID) {
-            uri.getQueryParameter("code")?.let { code ->
-                successResult(code)
-            } ?: run {
-                errorResult(uri.getQueryParameter("error") ?: "")
-            }
-            true
-        } else false
-    }
-
-    private fun successResult(code: String) {
-        val intent = Intent().apply {
-            putExtra(InfomaniakLogin.CODE_TAG, code)
-        }
-        setResult(RESULT_OK, intent)
-        finish()
-    }
-
-    private fun errorResult(errorCode: String) {
-        val intent = Intent().apply {
-            putExtra(InfomaniakLogin.ERROR_CODE_TAG, errorCode)
-            putExtra(InfomaniakLogin.ERROR_TRANSLATED_TAG, translateError(errorCode))
-        }
-        setResult(RESULT_OK, intent)
-        finish()
-    }
-
-    private fun translateError(errorCode: String): String {
-        return when (errorCode) {
-            InfomaniakLogin.WEBVIEW_ERROR_CODE_INTERNET_DISCONNECTED -> getString(R.string.connection_error)
-            InfomaniakLogin.WEBVIEW_ERROR_CODE_CONNECTION_REFUSED -> getString(R.string.connection_error)
-            InfomaniakLogin.ERROR_ACCESS_DENIED -> getString(R.string.access_denied)
-            else -> getString(R.string.an_error_has_occurred)
-        }
-    }
-
-    private inner class LoginWebViewClient : WebViewClient() {
-
-        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-            val url = request?.url.toString()
-            return !isValidUrl(url)
-        }
-
-        @Deprecated("Only for API below 23")
-        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-            return !isValidUrl(url)
-        }
-
-        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) = with(binding) {
-            progressBar.progress = 0
-            progressBar.isVisible = true
-        }
-
-        override fun onPageFinished(view: WebView?, url: String?) = with(binding) {
-            progressBar.progress = 100
-            progressBar.isGone = true
-        }
-
-        override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
-            error?.certificate?.apply {
-                if (issuedBy?.cName == "localhost" && issuedTo?.cName == "localhost") return
-            }
-            errorResult(InfomaniakLogin.SSL_ERROR_CODE)
-        }
-
-        @RequiresApi(Build.VERSION_CODES.M)
-        override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
-            if (isValidUrl(request?.url.toString())) {
-                errorResult(error?.description?.toString() ?: "")
-            }
-        }
-
-        @Deprecated("Only for API below 23")
-        override fun onReceivedError(view: WebView?, errorCode: Int, description: String?, failingUrl: String?) {
-            if (isValidUrl(failingUrl)) {
-                errorResult(description ?: "")
-            }
-        }
-
-        override fun onReceivedHttpError(view: WebView?, request: WebResourceRequest?, errorResponse: WebResourceResponse?) {
-            if (request?.method == "GET") errorResult(InfomaniakLogin.HTTP_ERROR_CODE)
-        }
-    }
-
-    private inner class ProgressWebChromeClient : WebChromeClient() {
-
-        override fun onProgressChanged(view: WebView, newProgress: Int) = with(binding.progressBar) {
-            progress = newProgress
-            if (newProgress == 100) isGone = true
         }
     }
 
