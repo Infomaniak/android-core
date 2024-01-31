432e369f91732f26af52b1183febac35ccbad05a
Fabian Devel <fabian.devel@infomaniak.com>
2024-01-31 14:00:42 +0100

    Add updateWorker in Stores module

---
+++ Stores/src/main/java/WorkerUpdateManager.kt
@@ -0,0 +1,37 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.lib.stores
+
+import android.content.Context
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+
+class WorkerUpdateManager(appContext: Context) {
+
+    private val appUpdateManager = AppUpdateManagerFactory.create(appContext)
+    private val localSettings = StoresLocalSettings.getInstance(appContext)
+
+    fun installDownloadedUpdate(onInstallFailure: (Exception) -> Unit, onInstallSuccess: () -> Unit) {
+        localSettings.hasAppUpdateDownloaded = false
+        appUpdateManager.completeUpdate()
+            .addOnSuccessListener { onInstallSuccess() }
+            .addOnFailureListener {
+                localSettings.resetUpdateSettings()
+                onInstallFailure(it)
+            }
+    }
+}
