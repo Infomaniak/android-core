e1e5f594a224109f64497ba627cbaff697943919
Fabian Devel <fabian.devel@infomaniak.com>
2024-02-06 14:25:00 +0100

    Add basic activity and resources files

---
+++ Stores/src/main/java/com/infomaniak/lib/stores/updaterequired/UpdateRequiredActivity.kt
@@ -0,0 +1,61 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.lib.stores.updaterequired
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.addCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.navArgs
+import com.infomaniak.lib.stores.databinding.ActivityUpdateRequiredBinding
+import kotlin.system.exitProcess
+
+class UpdateRequiredActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityUpdateRequiredBinding.inflate(layoutInflater) }
+    private val navigationArgs: UpdateRequiredActivityArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?): Unit = with(binding) {
+        super.onCreate(savedInstanceState)
+        setContentView(root)
+
+        onBackPressedDispatcher.addCallback(this@UpdateRequiredActivity) {
+            finishAffinity()
+            exitProcess(0)
+        }
+
+        updateAppButton.apply {
+            if (navigationArgs.primaryColor != UNDEFINED_PRIMARY_COLOR) setBackgroundColor(navigationArgs.primaryColor)
+            setOnClickListener {
+                // TODO
+            }
+        }
+    }
+
+    companion object {
+        private const val UNDEFINED_PRIMARY_COLOR = 0
+
+        fun startUpdateRequiredActivity(context: Context, primaryColor: Int = UNDEFINED_PRIMARY_COLOR) {
+            Intent(context, UpdateRequiredActivity::class.java).apply {
+                val args = UpdateRequiredActivityArgs(primaryColor)
+                putExtras(args.toBundle())
+            }.also(context::startActivity)
+        }
+    }
+}
