d5a298b9b551f1d40c67a2165440d8b365d5b247
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-01-22 15:12:32 +0100

    feat(MyKSuite): Add basic theme color

---
+++ MykSuite/src/main/java/com/infomaniak/lib/myksuite/ui/theme/ColorsDark.kt
@@ -0,0 +1,31 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.lib.myksuite.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Palette
+private const val dark1 = 0xFF152123
+private const val dark2 = 0xFF2B383B
+private const val dark3 = 0xFF3C4F52
+private const val shark = 0xFF9F9F9F
+private const val rabbit = 0xFFF1F1F1
+
+val CustomDarkColorScheme = CustomColorScheme(
+    chipBackground = Color(dark2),
+)
---
+++ MykSuite/src/main/java/com/infomaniak/lib/myksuite/ui/theme/Theme.kt
@@ -0,0 +1,57 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.lib.myksuite.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+
+val LocalIsDarkMode = staticCompositionLocalOf { false }
+val LocalCustomColorScheme: ProvidableCompositionLocal<CustomColorScheme> = staticCompositionLocalOf { CustomColorScheme() }
+
+@Composable
+fun MyKSuiteTheme(
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    val customColors = if (isDarkTheme) CustomDarkColorScheme else CustomLightColorScheme
+    CompositionLocalProvider(
+        // LocalTextStyle provides Typography.bodyRegular,
+        LocalCustomColorScheme provides customColors,
+        LocalIsDarkMode provides isDarkTheme,
+    ) {
+        MaterialTheme(content = content)
+    }
+}
+
+object MyKSuiteTheme {
+    val typography = Typography
+    val colors: CustomColorScheme
+        @Composable
+        get() = LocalCustomColorScheme.current
+    val materialColors: ColorScheme
+        @Composable
+        get() = MaterialTheme.colorScheme
+}
+
+@Immutable
+data class CustomColorScheme(
+    val chipBackground: Color = Color.Unspecified,
+)
