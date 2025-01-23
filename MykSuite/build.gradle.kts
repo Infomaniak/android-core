616acbf02c3aeb75ae687bc5278a864bd551a370
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-01-23 13:54:59 +0100

    refactor(MyKSuite): Migrate to kotlin dsl gradle file

---
+++ MykSuite/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.library")
+    alias(core.plugins.kotlin.android)
+    alias(core.plugins.compose.compiler)
+}
+
+val coreCompileSdk: Int by rootProject.extra
+val coreMinSdk: Int by rootProject.extra
+val javaVersion: JavaVersion by rootProject.extra
+
+android {
+
+    namespace = "com.infomaniak.lib.myksuite"
+    compileSdk = coreCompileSdk
+
+    defaultConfig {
+        minSdk = coreMinSdk
+
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = javaVersion.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(core.androidx.core.ktx)
+
+    // Compose
+    implementation(platform(core.compose.bom))
+    implementation(core.compose.runtime)
+    debugImplementation(core.compose.ui.tooling)
+    implementation(core.compose.material3)
+    implementation(core.compose.ui)
+    implementation(core.compose.ui.tooling.preview)
+}
