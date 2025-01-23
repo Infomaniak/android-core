6766e8776767012d8826ceb56307c0e5b9ecbf22
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-01-23 16:04:33 +0100

    feat(MyKSuite): Override material colors for the bottomsheet

--- MykSuite/src/main/java/com/infomaniak/lib/myksuite/ui/theme/ColorsDark.kt
+++ MykSuite/src/main/java/com/infomaniak/lib/myksuite/ui/theme/ColorsDark.kt
@@ -17,6 +17,7 @@
  */
 package com.infomaniak.lib.myksuite.ui.theme
 
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.ui.graphics.Color
 
 // Palette
@@ -29,6 +30,11 @@ private const val rabbit = 0xFFF1F1F1
 // KSuite App colors
 private const val infomaniak = 0xFF4CB7FF
 private const val mailButton = 0xFFEF8BA4
+
+internal val DarkColorScheme = darkColorScheme(
+    onSurfaceVariant = Color(rabbit), // Used for Bottomsheet drag handle
+    surfaceContainerLow = Color(bat), // Used for bottom sheet backgrounds
+)
 
 internal val MyKSuiteDarkColors = MyKSuiteColors(
     primaryTextColor = Color(rabbit),
