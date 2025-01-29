e717013ba4453846416a021106afaff8c4f3cc83
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-01-29 16:19:34 +0100

    feat(MyKSuiteDashboard): Add avatar

--- MykSuite/src/main/java/com/infomaniak/lib/myksuite/ui/screens/MyKSuiteDashboardScreen.kt
+++ MykSuite/src/main/java/com/infomaniak/lib/myksuite/ui/screens/MyKSuiteDashboardScreen.kt
@@ -20,23 +20,31 @@ package com.infomaniak.lib.myksuite.ui.screens
 import android.content.res.Configuration
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import com.infomaniak.lib.myksuite.R
 import com.infomaniak.lib.myksuite.ui.components.MyKSuiteChip
 import com.infomaniak.lib.myksuite.ui.components.WeightOneSpacer
@@ -54,7 +62,9 @@ fun MyKSuiteDashboardScreen(
 
     Box {
         Image(
-            modifier = Modifier.fillMaxWidth().fillMaxHeight(0.45f),
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.45f),
             contentScale = ContentScale.FillBounds,
             imageVector = ImageVector.vectorResource(R.drawable.illu_dashboard_background),
             contentDescription = null,
@@ -73,7 +83,7 @@ fun MyKSuiteDashboardScreen(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.spacedBy(Margin.Mini)
                 ) {
-                    // TODO avatar
+                    UserAvatar(avatarUri)
                     Text(modifier = Modifier.weight(1.0f), text = userName, maxLines = 1, overflow = TextOverflow.Ellipsis)
                     MyKSuiteChip()
                 }
@@ -81,6 +91,51 @@ fun MyKSuiteDashboardScreen(
                 AppStorageQuotas(paddedModifier)
                 PaddedDivider(paddedModifier)
             }
+        }
+    }
+}
+
+@Composable
+private fun UserAvatar(avatarUri: String) {
+    val context = LocalContext.current
+    var shouldDisplayPreview by rememberSaveable(avatarUri) { mutableStateOf(true) }
+
+    Box {
+        if (shouldDisplayPreview) {
+            val imageRequest = remember(avatarUri) {
+                ImageRequest.Builder(context)
+                    .data(avatarUri)
+                    .crossfade(true)
+                    .build()
+            }
+
+            AsyncImage(
+                modifier = Modifier.size(Dimens.iconSize),
+                model = imageRequest,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                onError = { shouldDisplayPreview = false },
+            )
+        } else {
+            Image(
+                modifier = Modifier
+                    .size(Dimens.iconSize)
+                    .border(
+                        width = 1.dp,
+                        brush = Brush.linearGradient(
+                            0.0f to Color(0xFF1DDDFD),
+                            0.1f to Color(0xFF337CFF),
+                            0.3f to Color(0xFFA055FC),
+                            0.4f to Color(0xFFF34BBB),
+                            0.6f to Color(0xFFFD8C3D),
+                            start = Offset(0.0f, 10.0f),
+                            end = Offset(0.0f, 90.0f),
+                        ),
+                        shape = RoundedCornerShape(Dimens.largeCornerRadius),
+                    ),
+                imageVector = ImageVector.vectorResource(R.drawable.ic_person),
+                contentDescription = null,
+            )
         }
     }
 }
