1e8e10d002beb3786a829727c06085920f9aecfa
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2025-01-29 16:55:45 +0100

    chore: Add await extension for Call.enqueue

---
+++ src/main/java/com/infomaniak/lib/login/ext/OkHttpClientExt.kt
@@ -0,0 +1,48 @@
+/*
+ * Infomaniak Login - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.infomaniak.lib.login.ext
+
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+/**
+ * Suspend extension that allows suspend [Call.enqueue]
+ * @return [Response] or throw exception
+ */
+internal suspend fun Call.await() = suspendCancellableCoroutine { continuation ->
+
+    enqueue(object : Callback {
+        override fun onFailure(call: Call, e: IOException) {
+            if (continuation.isCancelled) return // Avoid raising CancellationException
+            continuation.resumeWithException(e)
+        }
+
+        override fun onResponse(call: Call, response: Response) {
+            continuation.resume(response)
+        }
+    })
+
+    continuation.invokeOnCancellation {
+        runCatching { this@await.cancel() }
+    }
+}
