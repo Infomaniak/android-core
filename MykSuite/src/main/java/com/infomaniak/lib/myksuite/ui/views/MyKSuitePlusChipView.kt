7978ed744ca7422ef8149e4c6cdf235a6af99f2f
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-01-31 08:22:11 +0100

    refactor(MyKSuiteChip): Use image with background instead of material Chip

--- MykSuite/src/main/java/com/infomaniak/lib/myksuite/ui/views/MyKSuitePlusChipView.kt
+++ MykSuite/src/main/java/com/infomaniak/lib/myksuite/ui/views/MyKSuitePlusChipView.kt
@@ -19,14 +19,9 @@ package com.infomaniak.lib.myksuite.ui.views
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.compose.runtime.Composable
-import com.infomaniak.lib.myksuite.ui.components.MyKSuitePlusChip
 
 class MyKSuitePlusChipView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
-) : BaseMyKSuiteChipView(context, attrs, defStyleAttr) {
-
-    override val chipContent = @Composable { MyKSuitePlusChip { onClick?.onClick(this) } }
-}
+) : BaseMyKSuiteChipView(context, attrs, defStyleAttr, isMyKSuitePlus = true)
