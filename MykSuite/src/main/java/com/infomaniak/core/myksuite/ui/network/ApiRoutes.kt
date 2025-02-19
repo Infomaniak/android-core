16ca1be3474f9126bef798eb4e6869ac65e5f98f
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-02-19 13:28:33 +0100

    chore(DynamicDashboard): Clean code

--- MykSuite/src/main/java/com/infomaniak/core/myksuite/ui/network/ApiRoutes.kt
+++ MykSuite/src/main/java/com/infomaniak/core/myksuite/ui/network/ApiRoutes.kt
@@ -21,7 +21,6 @@ object ApiRoutes {
 
     const val MANAGER_URL = "https://manager.infomaniak.com/v3/ng/home"
 
-    private const val PREPROD_BASE_URL = "https://api.staging-myksuite.dev.infomaniak.ch"
     private const val BASE_URL = "https://api.infomaniak.com"
 
     fun myKSuiteData() = "$BASE_URL/1/my_ksuite/current?with=drive,mail,pack,can_trial,has_auto_renew"
