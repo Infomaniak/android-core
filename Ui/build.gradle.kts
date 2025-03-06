2e343eefedaad74084b84661be5d4a50cb0016a2
TommyDL-Infomaniak <tommy.diluna@infomaniak.com>
2025-03-06 11:33:33 +0100

    WIP: Initial work on Ui module

---
+++ Ui/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.library")
+    alias(core.plugins.kotlin.android)
+    alias(core.plugins.compose.compiler)
+}
+
+val coreCompileSdk: Int by rootProject.extra
+val legacyMinSdk: Int by rootProject.extra
+val javaVersion: JavaVersion by rootProject.extra
+
+android {
+
+    namespace = "com.infomaniak.core.ui"
+    compileSdk = coreCompileSdk
+
+    defaultConfig {
+        minSdk = legacyMinSdk
+
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = javaVersion.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(core.androidx.core.ktx)
+    implementation(core.material)
+}
