dc9beae9778d6b73a89fd6c4fe8cca51c92f2704
Louis CAD <louis.cognault@infomaniak.com>
2025-03-17 13:13:31 +0100

    chore: Fix project name in copyright notices

--- src/main/kotlin/com/infomaniak/core/Result.kt
+++ src/main/kotlin/com/infomaniak/core/Result.kt
@@ -1,5 +1,5 @@
 /*
- * Infomaniak SwissTransfer - Android
+* Infomaniak Core - Android
  * Copyright (C) 2025 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
