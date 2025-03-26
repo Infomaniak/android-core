ea7ca5e78cca7a41596124d15b97689fafc63dd5
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-03-26 16:19:23 +0100

    chore(AvatarModule): Doesn't remember image request

--- UserAvatar/src/main/java/com/infomaniak/core/useravatar/exposed/UserAvatar.kt
+++ UserAvatar/src/main/java/com/infomaniak/core/useravatar/exposed/UserAvatar.kt
@@ -24,8 +24,11 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -62,13 +65,11 @@ fun UserAvatar(modifier: Modifier = Modifier, avatarData: AvatarData, border: Bo
             AvatarDisplayState.Initials -> InitialsTextAvatar(avatarData)
             AvatarDisplayState.UnknownUser -> UnknownUserIcon(avatarData.iconColor)
             AvatarDisplayState.Avatar -> {
-                val context = LocalContext.current
-                val imageRequest = remember(avatarData.uri, context) {
-                    ImageRequest.Builder(context)
-                        .data(avatarData.uri)
-                        .crossfade(true)
-                        .build()
-                }
+                val imageRequest = ImageRequest.Builder(LocalContext.current)
+                    .data(avatarData.uri)
+                    .crossfade(true)
+                    .build()
+
                 AsyncImage(
                     model = imageRequest,
                     contentDescription = null,
