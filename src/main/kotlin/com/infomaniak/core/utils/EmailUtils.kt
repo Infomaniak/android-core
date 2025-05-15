8e717b17354112f362958d7be44e20be9f7f23ad
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-05-15 10:36:04 +0200

    feat: Use new valid email detection regex that matches closer to RFC5321
    The regex comes from what iOS was using in mail. Thanks to this update, we support more valid email adresses and we are more iso

--- src/main/kotlin/com/infomaniak/core/utils/EmailUtils.kt
+++ src/main/kotlin/com/infomaniak/core/utils/EmailUtils.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak Core - Android
- * Copyright (C) 2024 Infomaniak Network SA
+ * Copyright (C) 2024-2025 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,14 @@
  */
 package com.infomaniak.core.utils
 
-import android.util.Patterns
 
-fun String.isValidEmail(): Boolean = Patterns.EMAIL_ADDRESS.matcher(trim()).matches()
+/**
+ * This regex should be closer to RFC5321 compared to Patterns.EMAIL_ADDRESS which is missing, amongst many things, the
+ * characters #&~!^`{}/=$*?|
+ *
+ * This regex comes from the iOS mail app that supports more email addresses than Android did with Patterns.EMAIL_ADDRESS
+ */
+private val validEmailPattern =
+    "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])".toRegex()
+
+fun String.isValidEmail(): Boolean = validEmailPattern.matches(this)
