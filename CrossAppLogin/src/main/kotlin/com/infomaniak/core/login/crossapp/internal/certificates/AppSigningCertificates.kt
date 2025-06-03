efb6dc87244538b15033af8e2b1418e97e5f6507
Louis CAD <louis.cognault@infomaniak.com>
2025-06-03 17:36:18 +0200

    feat(CrossAppLogin): Add app signing certificate checking code

---
+++ CrossAppLogin/src/main/kotlin/com/infomaniak/core/login/crossapp/internal/certificates/AppSigningCertificates.kt
@@ -0,0 +1,40 @@
+/*
+ * Infomaniak Login - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.infomaniak.core.login.crossapp.internal.certificates
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.invoke
+import java.security.MessageDigest
+import android.content.pm.Signature as SigningCertificate
+
+internal class AppSigningCertificates(
+    builderAction: MutableMap<String, Set<LazyAppSigningCertificate>>.() -> Unit
+) {
+
+    private val certificatesMapForPackages: Map<String, Set<LazyAppSigningCertificate>> = buildMap(builderAction)
+
+    suspend fun matches(targetPackageName: String, signingCertificate: SigningCertificate): Boolean {
+        val acceptedCertificates = certificatesMapForPackages[targetPackageName] ?: return false
+        return Dispatchers.Default {
+            val givenSha256 = signingCertificate.sha256()
+            acceptedCertificates.any { it.matches(givenSha256) }
+        }
+    }
+
+    private fun SigningCertificate.sha256(): ByteArray = MessageDigest.getInstance("SHA-256").digest(toByteArray())
+}
