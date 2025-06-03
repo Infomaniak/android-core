bc9cf45f9dabf67278782775243f656b7af9394f
Louis CAD <louis.cognault@infomaniak.com>
2025-06-03 13:50:34 +0200

    chore(CrossAppLogin): Create CrossAppLogin module

---
+++ CrossAppLogin/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.library")
+    alias(core.plugins.kotlin.android)
+}
+
+val coreCompileSdk: Int by rootProject.extra
+val coreMinSdk: Int by rootProject.extra
+val javaVersion: JavaVersion by rootProject.extra
+
+android {
+    namespace = "com.infomaniak.core.login.crossapp"
+    compileSdk = coreCompileSdk
+
+    defaultConfig {
+        minSdk = coreMinSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = javaVersion.toString()
+    }
+}
+
+dependencies {
+    api(core.kotlinx.coroutines.core)
+    api(core.androidx.lifecycle.runtime.ktx)
+    api(core.androidx.lifecycle.service)
+    api(core.kotlinx.serialization.protobuf)
+
+    implementation(core.splitties.mainthread)
+
+    testImplementation(core.junit)
+    androidTestImplementation(core.androidx.junit)
+}
