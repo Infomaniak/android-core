71f7afb30e791abda39c889a2d4ebd9c0b2d3220
Louis CAD <louis.cognault@infomaniak.com>
2025-06-05 16:46:10 +0200

    feat: Add withBoundService

---
+++ src/main/kotlin/com/infomaniak/core/android/service/BoundServices.kt
@@ -0,0 +1,184 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+@file:OptIn(ExperimentalSplittiesApi::class)
+
+package com.infomaniak.core.android.service
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
+import androidx.annotation.RequiresApi
+import com.infomaniak.core.Xor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.withContext
+import splitties.coroutines.raceOf
+import splitties.experimental.ExperimentalSplittiesApi
+
+/**
+ * Attempts to bind to the [service], runs [block] once connected, and cancels it on disconnection.
+ * Finally unbinds the service.
+ *
+ * This function is cancellable. It is recommended to use a proper scope that cancels when the
+ * connecting component is destroyed, or stopped.
+ *
+ * You may want to use this in a loop.
+ */
+@Throws(SecurityException::class)
+suspend fun <R> Context.withBoundService(
+    service: Intent,
+    timeoutConnection: suspend (isWaitingForReconnect: Boolean) -> ServiceConnectionTimeoutBehavior<R>,
+    onDisconnected: () -> OnServiceDisconnectionBehavior<R>,
+    onBindingIssue: suspend (issue: ServiceBindingIssue) -> OnBindingIssueBehavior<R>,
+    flags: Int = Context.BIND_AUTO_CREATE,
+    block: suspend (serviceBinder: IBinder) -> R
+): R {
+    val binderChannel = Channel<IBinder>(capacity = Channel.CONFLATED)
+    val onDisconnectChannel = Channel<OnServiceDisconnectionBehavior<R>>(capacity = Channel.CONFLATED)
+    val bindingIssueChannel = Channel<ServiceBindingIssue>(capacity = Channel.CONFLATED)
+
+    val connection = object : ServiceConnection {
+
+        @RequiresApi(28)
+        override fun onNullBinding(name: ComponentName) {
+            bindingIssueChannel.trySend(ServiceBindingIssue.NullBinding)
+        }
+
+        @RequiresApi(26)
+        override fun onBindingDied(name: ComponentName) {
+            bindingIssueChannel.trySend(ServiceBindingIssue.BindingDied)
+        }
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder?) {
+            when (service) {
+                null -> bindingIssueChannel.trySend(ServiceBindingIssue.NullBinding)
+                else -> binderChannel.trySend(service)
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            val onDisconnectedBehavior = onDisconnected()
+            onDisconnectChannel.trySend(onDisconnectedBehavior)
+        }
+    }
+    bindLoop@ while (true) {
+        try {
+            val serviceBindingStarted = withContext(NonCancellable + Dispatchers.IO) {
+                bindService(service, connection, flags)
+            }
+            if (serviceBindingStarted.not()) {
+                val bindingIssueBehavior = onBindingIssue(ServiceBindingIssue.NotFoundOrNoPermission)
+                when (bindingIssueBehavior) {
+                    is OnBindingIssueBehavior.GiveUp<R> -> return bindingIssueBehavior.returnValue
+                    OnBindingIssueBehavior.Retry -> continue@bindLoop
+                }
+            }
+            var isWaitingForReconnect = false
+            reconnectLoop@ while (true) {
+                val bindingOrElse: Xor<IBinder, TimeoutOrIssue<R>> = raceOf({
+                    Xor.First(binderChannel.receive())
+                }, {
+                    // Sometimes, onServiceConnected is never called.
+                    // This happens often when the app has just been updated, so we use a timeout.
+                    val timeoutBehavior = timeoutConnection(isWaitingForReconnect)
+                    Xor.Second(Xor.First(timeoutBehavior))
+                }, {
+                    val bindingIssue = bindingIssueChannel.receive()
+                    val bindingIssueBehavior = onBindingIssue(bindingIssue)
+                    Xor.Second(Xor.Second(bindingIssueBehavior))
+                })
+
+                val binding = when (bindingOrElse) {
+                    is Xor.First<IBinder> -> bindingOrElse.value
+                    is Xor.Second<TimeoutOrIssue<R>> -> when (val issue = bindingOrElse.value) {
+                        is Xor.First<ServiceConnectionTimeoutBehavior<R>> -> when (issue.value) {
+                            is ServiceConnectionTimeoutBehavior.GiveUp<R> -> return issue.value.returnValue
+                            ServiceConnectionTimeoutBehavior.UnbindAndRetry -> continue@bindLoop
+                        }
+                        is Xor.Second<OnBindingIssueBehavior<R>> -> when (issue.value) {
+                            is OnBindingIssueBehavior.GiveUp<R> -> return issue.value.returnValue
+                            OnBindingIssueBehavior.Retry -> continue@bindLoop
+                        }
+                    }
+                }
+                val result: Xor<R, ServiceBindingIssue?> = raceOf({
+                    Xor.First(block(binding))
+                }, {
+                    val onDisconnectBehavior = onDisconnectChannel.receive()
+                    when (onDisconnectBehavior) {
+                        is OnServiceDisconnectionBehavior.AwaitRebind<R> -> raceOf({
+                            val result = onDisconnectBehavior.awaitUnbind()
+                            Xor.First(result)
+                        }, {
+                            val newConnectionBinder = binderChannel.receive()
+                            binderChannel.send(newConnectionBinder) // Resend it so the block can get it back.
+                            Xor.Second(null)
+                        })
+                        is OnServiceDisconnectionBehavior.UnbindImmediately<R> -> {
+                            Xor.First(onDisconnectBehavior.returnValue)
+                        }
+                    }
+                })
+                return when (result) {
+                    is Xor.First<R> -> result.value
+                    is Xor.Second<ServiceBindingIssue?> -> when (result.value) {
+                        null -> {
+                            isWaitingForReconnect = true
+                            continue@reconnectLoop
+                        }
+                        else -> when (val behavior = onBindingIssue(result.value)) {
+                            is OnBindingIssueBehavior.Retry -> continue@bindLoop
+                            is OnBindingIssueBehavior.GiveUp<R> -> behavior.returnValue
+                        }
+                    }
+                }
+            }
+        } finally {
+            withContext(NonCancellable + Dispatchers.IO) {
+                unbindService(connection)
+                unbindService(connection) // Try to see if it's idempotent. //TODO: Remove this once we know.
+            }
+        }
+    }
+}
+
+private typealias TimeoutOrIssue<R> = Xor<ServiceConnectionTimeoutBehavior<R>, OnBindingIssueBehavior<R>>
+
+sealed interface OnServiceDisconnectionBehavior<R> {
+    data class AwaitRebind<R>(internal val awaitUnbind: suspend () -> R) : OnServiceDisconnectionBehavior<R>
+    data class UnbindImmediately<R>(internal val returnValue: R) : OnServiceDisconnectionBehavior<R>
+}
+
+sealed interface OnBindingIssueBehavior<R> {
+    data object Retry : OnBindingIssueBehavior<Nothing>
+    data class GiveUp<R>(internal val returnValue: R) : OnBindingIssueBehavior<R>
+}
+
+sealed interface ServiceConnectionTimeoutBehavior<R> {
+    data object UnbindAndRetry : ServiceConnectionTimeoutBehavior<Nothing>
+    data class GiveUp<R>(internal val returnValue: R) : ServiceConnectionTimeoutBehavior<R>
+}
+
+enum class ServiceBindingIssue {
+    NotFoundOrNoPermission,
+    BindingDied,
+    NullBinding,
+}
