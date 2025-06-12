876605ae476476f44583aed668359d07e74258d4
Vincent TE <vincent.te@infomaniak.com>
2025-06-12 14:59:55 +0200

    feat: Add custom rule for trailing coma in classes and method

--- KtlintCustomRules/src/main/java/com/infomaniak/core/ktlint/customrules/CustomRuleSetProvider.kt
+++ KtlintCustomRules/src/main/java/com/infomaniak/core/ktlint/customrules/CustomRuleSetProvider.kt
@@ -26,6 +26,6 @@ internal const val CUSTOM_RULE_SET_ID = "custom-rule-set-id"
 class CustomRuleSetProvider : RuleSetProviderV3(RuleSetId(CUSTOM_RULE_SET_ID)) {
     override fun getRuleProviders(): Set<RuleProvider> =
         setOf(
-            RuleProvider { NoVarRule() },
+            RuleProvider { TrailingCommaInMultilineParametersRule() },
         )
 }
