8b544e490423a542f60096e7b3df028794d22055
TommyDL-Infomaniak <tommy.diluna@infomaniak.com>
2025-06-23 16:03:07 +0200

    feat: Add Auth module

---
+++ Auth/src/main/kotlin/com/infomaniak/core/auth/TokenAuthenticator.kt
@@ -0,0 +1,72 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2022-2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.auth
+
+import com.infomaniak.core.auth.utils.ApiTokenExt.isInfinite
+import com.infomaniak.lib.login.ApiToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+import com.infomaniak.core.auth.api.ApiController as AuthApiController
+
+class TokenAuthenticator(
+    private val tokenInterceptorListener: TokenInterceptorListener
+) : Authenticator {
+
+    private val userId = tokenInterceptorListener.getCurrentUserId()
+
+    override fun authenticate(route: Route?, response: Response): Request? {
+        return runBlocking(Dispatchers.IO) {
+            mutex.withLock {
+                val request = response.request
+                val authorization = request.header("Authorization")
+                val apiToken = tokenInterceptorListener.getApiToken() ?: return@runBlocking null
+                val isAlreadyRefreshed = apiToken.accessToken != authorization?.replaceFirst("Bearer ", "")
+                val hasUserChanged = userId != tokenInterceptorListener.getCurrentUserId()
+
+                return@runBlocking when {
+                    hasUserChanged -> null
+                    apiToken.isInfinite -> {
+                        tokenInterceptorListener.onRefreshTokenError()
+                        null
+                    }
+                    isAlreadyRefreshed -> changeAccessToken(request, apiToken)
+                    else -> {
+                        val newToken = AuthApiController.refreshToken(apiToken.refreshToken!!, tokenInterceptorListener)
+                        changeAccessToken(request, newToken)
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        val mutex = Mutex()
+
+        fun changeAccessToken(request: Request, apiToken: ApiToken): Request {
+            val builder = request.newBuilder()
+            builder.header("Authorization", "Bearer ${apiToken.accessToken}")
+            return builder.build()
+        }
+    }
+}
---
+++ Auth/src/main/kotlin/com/infomaniak/core/auth/TokenInterceptor.kt
@@ -0,0 +1,69 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2022-2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.auth
+
+import com.infomaniak.core.auth.TokenAuthenticator.Companion.changeAccessToken
+import com.infomaniak.core.network.api.ApiController.json
+import com.infomaniak.core.network.api.ApiController.toApiError
+import com.infomaniak.core.network.api.InternalTranslatedErrorCode
+import com.infomaniak.core.network.models.ApiResponse
+import com.infomaniak.core.network.models.ApiResponseStatus
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Protocol
+import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
+
+class TokenInterceptor(
+    private val tokenInterceptorListener: TokenInterceptorListener
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+
+        runBlocking(Dispatchers.Default) {
+            tokenInterceptorListener.getApiToken()
+        }?.let { apiToken ->
+            val authorization = request.header("Authorization")
+            if (apiToken.accessToken != authorization?.replaceFirst("Bearer ", "")) {
+                request = changeAccessToken(request, apiToken)
+            }
+        } ?: return userLoggedOutResponse(chain)
+
+        return chain.proceed(request)
+    }
+
+    private fun userLoggedOutResponse(chain: Interceptor.Chain): Response {
+        val errorBody = ApiResponse<Unit>(
+            result = ApiResponseStatus.ERROR,
+            error = InternalTranslatedErrorCode.UserLoggedOut.toApiError()
+        )
+        return Response.Builder()
+            .code(401)
+            .request(chain.request())
+            .protocol(Protocol.HTTP_2)
+            .message(USER_LOGGED_OUT_MESSAGE)
+            .body(json.encodeToString(errorBody).toResponseBody())
+            .build()
+    }
+
+    companion object {
+        const val USER_LOGGED_OUT_MESSAGE = "User is logged out custom message"
+    }
+}
