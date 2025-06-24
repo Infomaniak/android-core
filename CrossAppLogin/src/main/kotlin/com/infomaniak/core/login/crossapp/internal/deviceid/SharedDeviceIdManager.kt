928aae77c2c20a3a565ef475e1b351a8d222c525
Louis CAD <louis.cognault@infomaniak.com>
2025-06-24 13:54:57 +0200

    feat(CrossAppLogin): Implement cross-app device id logic

---
+++ CrossAppLogin/src/main/kotlin/com/infomaniak/core/login/crossapp/internal/deviceid/SharedDeviceIdManager.kt
@@ -0,0 +1,49 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.login.crossapp.internal.deviceid
+
+import android.content.Context
+import com.infomaniak.core.login.crossapp.internal.certificates.AppCertificateChecker
+import kotlinx.coroutines.sync.Mutex
+
+sealed interface SharedDeviceIdManager {
+
+    companion object {
+        internal val sharedDeviceIdMutex = Mutex()
+        internal val storage: SharedDeviceIdStorage = SharedDeviceIdStorage
+
+        internal operator fun invoke(
+            context: Context,
+            certificateChecker: AppCertificateChecker,
+            targetPackageNames: Set<String>
+        ): SharedDeviceIdManager = SharedDeviceIdManagerImpl(
+            context,
+            certificateChecker,
+            targetPackageNames
+        )
+    }
+
+    /**
+     * Returns an id for this device, that is shared across all our apps.
+     *
+     * This might involve generating said id.
+     */
+    suspend fun getCrossAppDeviceId(): String
+
+    suspend fun findCrossAppDeviceId(packageNamesToSkip: Set<String>): String?
+}
