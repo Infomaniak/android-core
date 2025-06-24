fdfbb8b037809332084f4f8f139c67b78081a4fe
Morgane FAREZ <morgane.farez@infomaniak.com>
2025-06-24 14:49:14 +0200

    feat: Add sentry configuration to Core

---
+++ SentryConfig/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+val coreCompileSdk: Int by rootProject.extra
+val coreMinSdk: Int by rootProject.extra
+val javaVersion: JavaVersion by rootProject.extra
+
+android {
+    namespace = "com.infomaniak.core.sentryconfig"
+    compileSdk = coreCompileSdk
+
+    defaultConfig {
+        minSdk = coreMinSdk
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = javaVersion.toString()
+    }
+}
+
+dependencies {
+    implementation(project(":Core:Network"))
+    implementation(core.sentry.android)
+    implementation(core.sentry.android.fragment)
+}
---
+++ SentryConfig/src/main/java/com/infomaniak/core/sentryconfig/SentryConfig.kt
@@ -0,0 +1,74 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.sentryconfig
+
+import android.app.Application
+import android.content.Context
+import com.infomaniak.core.network.api.ApiController
+import io.sentry.SentryEvent
+import io.sentry.SentryOptions
+import io.sentry.android.core.SentryAndroid
+import io.sentry.android.core.SentryAndroidOptions
+import io.sentry.android.fragment.FragmentLifecycleIntegration
+import io.sentry.android.fragment.FragmentLifecycleState
+
+class SentryConfig {
+    fun configureSentry(
+        isDebug: Boolean,
+        isSentryTrackingEnabled: Boolean,
+        context: Context,
+        application: Application,
+        isErrorException: (Throwable?) -> Boolean
+    ) {
+        SentryAndroid.init(context) { options: SentryAndroidOptions ->
+            // Register the callback as an option
+            options.beforeSend = SentryOptions.BeforeSendCallback { event: SentryEvent, _: Any? ->
+                val exception = event.throwable
+                /**
+                 * Reasons to discard Sentry events :
+                 * - Application is in Debug mode
+                 * - User deactivated Sentry tracking in DataManagement settings
+                 * - The exception was an [ApiController.NetworkException], and we don't want to send them to Sentry
+                 * - The exception was an [ApiErrorException] with an [ErrorCode.ACCESS_DENIED] or
+                 *   [ErrorCode.NOT_AUTHORIZED] error code, and we don't want to send them to Sentry
+                 */
+                when {
+                    isDebug -> null
+                    !isSentryTrackingEnabled -> null
+                    exception is ApiController.NetworkException -> null
+                    isErrorException(exception) -> null
+                    else -> event
+                }
+            }
+            options.addIntegration(
+                FragmentLifecycleIntegration(
+                    application = application,
+                    filterFragmentLifecycleBreadcrumbs = setOf(
+                        FragmentLifecycleState.CREATED,
+                        FragmentLifecycleState.STARTED,
+                        FragmentLifecycleState.RESUMED,
+                        FragmentLifecycleState.PAUSED,
+                        FragmentLifecycleState.STOPPED,
+                        FragmentLifecycleState.DESTROYED,
+                    ),
+                    enableAutoFragmentLifecycleTracing = true,
+                )
+            )
+        }
+    }
+}
