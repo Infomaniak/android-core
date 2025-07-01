ab81e400d2cf0c3d99e56504593ffd841906bdae
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2025-07-01 09:04:11 +0200

    feat: Use new architecture to communicate between app & module

--- CrossAppLoginUI/src/main/kotlin/com/infomaniak/core/crossloginui/components/PrimaryButton.kt
+++ CrossAppLoginUI/src/main/kotlin/com/infomaniak/core/crossloginui/components/PrimaryButton.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.infomaniak.core.crossloginui.screens.components
+package com.infomaniak.core.crossloginui.components
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
