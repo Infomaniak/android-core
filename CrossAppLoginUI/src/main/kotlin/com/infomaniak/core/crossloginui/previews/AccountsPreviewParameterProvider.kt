2a7ee6bdab66ff321e9b1f63bb5e3d55da8365e0
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2025-07-03 14:25:21 +0200

    fix: Add missing previews

---
+++ CrossAppLoginUI/src/main/kotlin/com/infomaniak/core/crossloginui/previews/AccountsPreviewParameterProvider.kt
@@ -0,0 +1,58 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.crossloginui.previews
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.infomaniak.core.crossloginui.data.CrossLoginUiAccount
+
+class AccountsPreviewParameter : PreviewParameterProvider<List<CrossLoginUiAccount>> {
+    override val values: Sequence<List<CrossLoginUiAccount>> = sequenceOf(accountsPreviewData)
+}
+
+val accountsPreviewData = listOf(
+    CrossLoginUiAccount(
+        name = "Jane Doe",
+        email = "jane.doe@ik.me",
+        avatarUrl = "https://picsum.photos/id/237/200/200",
+        isSelected = true,
+    ),
+    CrossLoginUiAccount(
+        name = "John Doe",
+        email = "john.doe@ik.me",
+        avatarUrl = "https://picsum.photos/id/3/200/200",
+        isSelected = false,
+    ),
+    CrossLoginUiAccount(
+        name = "Gibran Cocodes",
+        email = "gibran.cocodes@ik.me",
+        avatarUrl = "https://picsum.photos/id/10/200/200",
+        isSelected = true,
+    ),
+    CrossLoginUiAccount(
+        name = "Kevin Mâchicoulis",
+        email = "kevin.mâchicoulis@ik.me",
+        avatarUrl = "https://picsum.photos/id/15/200/200",
+        isSelected = true,
+    ),
+    CrossLoginUiAccount(
+        name = "Tommy Del Sol",
+        email = "tommy.delsol@ik.me",
+        avatarUrl = "https://picsum.photos/id/20/200/200",
+        isSelected = true,
+    ),
+)
