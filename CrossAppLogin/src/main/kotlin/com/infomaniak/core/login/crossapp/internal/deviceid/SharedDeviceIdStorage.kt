c119a377502e528960777c3b27c14b654448bc3d
Louis CAD <louis.cognault@infomaniak.com>
2025-07-14 13:35:53 +0200

    chore(CrossAppLogin): Ensure stale cross app device id can't be used

--- CrossAppLogin/src/main/kotlin/com/infomaniak/core/login/crossapp/internal/deviceid/SharedDeviceIdStorage.kt
+++ CrossAppLogin/src/main/kotlin/com/infomaniak/core/login/crossapp/internal/deviceid/SharedDeviceIdStorage.kt
@@ -17,32 +17,43 @@
  */
 package com.infomaniak.core.login.crossapp.internal.deviceid
 
+import android.provider.Settings
+import android.provider.Settings.Secure.ANDROID_ID
 import androidx.core.util.AtomicFile
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.invoke
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromByteArray
+import kotlinx.serialization.encodeToByteArray
+import kotlinx.serialization.protobuf.ProtoBuf
+import kotlinx.serialization.protobuf.ProtoNumber
 import splitties.init.appCtx
 import java.io.FileNotFoundException
 import java.io.IOException
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalSerializationApi::class)
 @ExperimentalUuidApi
 internal object SharedDeviceIdStorage {
 
-    private val dataFile = AtomicFile(appCtx.filesDir.resolve("generatedSharedDeviceId.utf8"))
+    private val dataFile = AtomicFile(appCtx.filesDir.resolve("generatedSharedDeviceId"))
 
     private val localDataReadWriteMutex = Mutex()
-
-    //TODO: Store ANDROID_ID or something to compare with current before returning,
-    // so we never return a stale backed-up id.
 
     @Throws(IOException::class)
     suspend fun readDeviceId(): Uuid? = Dispatchers.IO {
         localDataReadWriteMutex.withLock {
             try {
-                dataFile.openRead().use { stream -> Uuid.fromByteArray(stream.readBytes()) }
+                dataFile.openRead().use { stream ->
+                    ProtoBuf.decodeFromByteArray<SharedDeviceId>(stream.readBytes()).takeUnless {
+                        it.androidId != getAndroidId()
+                        // If different, the value was restored from a backup of another device (or post-reset).
+                    }?.let { sharedId -> Uuid.fromByteArray(sharedId.uuid) }
+                }
             } catch (_: FileNotFoundException) {
                 null
             }
@@ -53,10 +64,14 @@ internal object SharedDeviceIdStorage {
     suspend fun setDeviceId(sharedDeviceId: Uuid) {
         Dispatchers.IO {
             localDataReadWriteMutex.withLock {
-                //TODO: Check that we don't have it already, and report if we do?
                 dataFile.startWrite().use { outputStream ->
                     try {
-                        outputStream.write(sharedDeviceId.toByteArray())
+                        val data = SharedDeviceId(
+                            androidId = getAndroidId(),
+                            uuid = sharedDeviceId.toByteArray()
+                        ).let { ProtoBuf.encodeToByteArray(it) }
+
+                        outputStream.write(data)
                         dataFile.finishWrite(outputStream)
                     } catch (t: Throwable) {
                         dataFile.failWrite(outputStream)
@@ -65,5 +80,30 @@ internal object SharedDeviceIdStorage {
                 }
             }
         }
+    }
+
+    /**
+     * ## Important message to editors
+     *
+     * For all existing Unless the given field never made it to a public release (which includes betas!):
+     * - **NEVER** change the type of an existing field.
+     * - **NEVER** change the value in the `@ProtoNumber` of an existing field.
+     * - **NEVER** remove a field that was present before.
+     * - **NEVER** reuse a protobuf number. Those shall be unique and stable, essentially forever.
+     * - **Safe changes:**
+     *    1. Renaming any existing field.
+     *    2. Adding new fields with explicit `@ProtoNumber` using a new, never used number.
+     *    3. Deprecating any field (without touching its `@ProtoNumber`).
+     *    4. Rename this data class.
+     */
+    @Serializable
+    private class SharedDeviceId(
+        @ProtoNumber(1) val androidId: String,
+        @ProtoNumber(2) val uuid: ByteArray,
+    )
+
+    private suspend fun getAndroidId() = Dispatchers.IO {
+        @Suppress("HardwareIds")
+        Settings.Secure.getString(appCtx.contentResolver, ANDROID_ID)
     }
 }
