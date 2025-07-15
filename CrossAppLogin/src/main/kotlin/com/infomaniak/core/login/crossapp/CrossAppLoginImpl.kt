6b5f285a79d2195c9a0358acc2950afb39fdfbf2
Louis CAD <louis.cognault@infomaniak.com>
2025-07-15 14:56:52 +0200

    chore(CrossAppLogin): Remove no longer needed TODO comments

--- CrossAppLogin/src/main/kotlin/com/infomaniak/core/login/crossapp/CrossAppLoginImpl.kt
+++ CrossAppLogin/src/main/kotlin/com/infomaniak/core/login/crossapp/CrossAppLoginImpl.kt
@@ -77,28 +77,6 @@ internal class CrossAppLoginImpl(private val context: Context) : CrossAppLogin {
         targetPackageNames = targetPackageNames.toSet()
     )
 
-        // TODO:
-        //  1. If we already have an id, return it, otherwise, continue.
-        //  2. Acquire `sharedDeviceIdLock`
-        //  3. If we now have an id, return it, otherwise, continue.
-        //  4. Generate id for proposal.
-        //  5. Contact all known apps with the "START_SHARED_DEVICE_ID_SYNC" message, giving them the list of apps we're reaching //REMOVE THIS?, and the id proposal
-        //    a. If we already have an id, return it, otherwise, continue.
-        //    b. Acquire our `sharedDeviceIdLock`
-        //    c. If we now have an id, return it (releasing the sharedDeviceLock), otherwise, continue.
-        //    d. If there are other other apps that we know but didn't appear in the given list: ask them and return the result combination.
-        //    e. Otherwise return that we don't have the id, and wait (with timeout for the finish event with the id to persist).
-        //  6. Wait until one app gives us an id, or until all apps reply they don't have it
-        //  7. Persist our sharedDeviceId to the returned one (if a contacted app gave us one), or to the one we initially generated.
-        //  8. Release `sharedDeviceIdLock`
-        //  9. Send "FINISH_SHARED_DEVICE_ID_SYNC" to all apps that didn't have the shared device id already, with the id and the entire list of checked apps.
-        //    a. Persist the given shared device id.
-        //    b. Release `sharedDeviceIdLock`
-        //    c. Forward the message to all apps we know that weren't checked and didn't have the shared id yet.
-
-        // TODO: Handle non updated apps (ignore them).
-        // TODO: Handle apps being updated, by retrying if we detect the lastUpdateTime changed, or if we find a packageInstaller update session.
-
     @ExperimentalSerializationApi
     private suspend fun retrieveAccountsFromApp(packageName: String): List<ExternalAccount> = raceOf(
         {
