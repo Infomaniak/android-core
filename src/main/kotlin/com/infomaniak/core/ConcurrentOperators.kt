06260204a9d685ae2360347467e7d1eac40ffee8
Louis CAD <louis.cognault@infomaniak.com>
2025-07-17 11:47:53 +0200

    feat(CrossAppLogin): Sending cross-app device id

--- src/main/kotlin/com/infomaniak/core/ConcurrentOperators.kt
+++ src/main/kotlin/com/infomaniak/core/ConcurrentOperators.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak Core - Android
- * Copyright (C) 2022-2024 Infomaniak Network SA
+ * Copyright (C) 2025 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,17 +15,16 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.infomaniak.lib.core.api
+package com.infomaniak.core
 
-import com.infomaniak.lib.core.BuildConfig.INFOMANIAK_API
-
-object ApiRoutesCore {
-
-    fun getUserProfile(): String {
-        return "${INFOMANIAK_API}profile?no_avatar_default=1"
-    }
-
-    fun sendDeviceInfo(): String {
-        return "${INFOMANIAK_API}devices"
+suspend fun <T> Iterable<T>.allConcurrent(
+    predicate: suspend (T) -> Boolean
+): Boolean {
+    if (this is Collection && isEmpty()) return true
+    return completableScope { completable ->
+        for (element in this@allConcurrent) {
+            if (!predicate(element)) completable.complete(false)
+        }
+        true
     }
 }
