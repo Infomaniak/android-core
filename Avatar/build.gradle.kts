0d1b42a4c118d734074bcaf4168dd5d9dda04b41
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-07-22 10:35:19 +0200

    chore: Remove unused commented out dependencies

--- Avatar/build.gradle.kts
+++ Avatar/build.gradle.kts
@@ -31,11 +31,8 @@ android {
 }
 
 dependencies {
-    // implementation(core.androidx.core.ktx)
-
     implementation(platform(core.compose.bom))
     implementation(core.compose.runtime)
-    // implementation(core.compose.material3)
     implementation(core.compose.ui)
     debugImplementation(core.compose.ui.tooling)
     implementation(core.compose.ui.tooling.preview)
