cdad0f3852c424c4d6efec6198bc18f53f8a3a4a
Louis CAD <louis.cognault@infomaniak.com>
2025-07-28 11:41:53 +0200

    feat: Introduce higher level extension flowOnNewHandlerThread

--- src/main/kotlin/com/infomaniak/core/Flow.kt
+++ src/main/kotlin/com/infomaniak/core/Flow.kt
@@ -19,8 +19,10 @@
 
 package com.infomaniak.core
 
+import android.os.Process
 import android.os.SystemClock
 import kotlinx.coroutines.CloseableCoroutineDispatcher
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
@@ -58,6 +60,37 @@ fun <T> Flow<T>.rateLimit(minInterval: Duration): Flow<T> = channelFlow {
         lastEmitElapsedNanos = SystemClock.elapsedRealtimeNanos()
     }
 }.buffer(Channel.RENDEZVOUS)
+
+/**
+ * Every time the resulting flow is collected, it will create a new internal [android.os.HandlerThread],
+ * and when the flow is cancelled or completes, this thread will be quit (with [android.os.HandlerThread.quitSafely]).
+ *
+ * This is helpful when you need a given flow to be activated on a single, looper thread.
+ *
+ * Example usage:
+ * ```
+ * val someDataFlow: Flow<Something> = flow {
+ *     getSomeDatabaseInstance().use { db ->
+ *         val flow = db.someQuery()
+ *             .toFlow()
+ *             .map { it?.toSomethingElse() }
+ *         emitAll(flow)
+ *     }
+ * }.flowOnNewHandlerThread(name = "someData")
+ * ```
+ *
+ * Note that if [name] is left to `null`, the system will generate a default thread name.
+ *
+ * Also note that the JVM and ART allow multiple threads to have the same name.
+ * (Just keep it in mind if there's a need to analyze which thread is doing what.)
+ */
+fun <T> Flow<T>.flowOnNewHandlerThread(
+    name: String? = null,
+    priority: Int = Process.THREAD_PRIORITY_DEFAULT
+): Flow<T> = flowOnLazyClosable {
+    @OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+    HandlerThreadDispatcher(name = name, priority = priority)
+}
 
 /**
  * Similar to [flowOn], but will lazily create the Dispatcher returned by
