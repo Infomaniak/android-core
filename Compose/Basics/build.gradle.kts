f5dd04ffefdf07a113f59786fa2e927c030334ba
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-08-18 15:46:31 +0200

    chore: Move ThemedBottomSheetScaffold to core

--- Compose/Basics/build.gradle.kts
+++ Compose/Basics/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 
 dependencies {
     implementation(project(":Core"))
+    implementation(project(":Core:Compose:Margin"))
     implementation(platform(core.compose.bom))
     implementation(core.compose.material3)
     implementation(core.compose.runtime)
