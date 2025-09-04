e8a79c4be423a2ada7dc427f25609bc27f8f10fd
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2025-09-04 06:28:39 +0200

    refactor: Update features & strings

--- kSuite/kSuitePro/src/main/kotlin/com/infomaniak/core/ksuite/ksuitepro/data/ProFeature.kt
+++ kSuite/kSuitePro/src/main/kotlin/com/infomaniak/core/ksuite/ksuitepro/data/ProFeature.kt
@@ -23,68 +23,73 @@ import com.infomaniak.core.ksuite.ksuitepro.R
 
 internal enum class ProFeature(@StringRes val title: Int, @DrawableRes val icon: Int) {
 
-    StandardStorage(
-        title = R.string.kSuiteStandardStorageLabel,
-        icon = R.drawable.ic_cloud_disk,
-    ),
-
-    BusinessStorage(
-        title = R.string.kSuiteBusinessStorageLabel,
-        icon = R.drawable.ic_cloud_disk,
-    ),
-
-    EnterpriseStorage(
-        title = R.string.kSuiteEnterpriseStorageLabel,
-        icon = R.drawable.ic_cloud_disk,
-    ),
-
-    StandardChat(
-        title = R.string.kSuiteStandardKChatLabel,
-        icon = R.drawable.ic_speech_bubble,
-    ),
-
-    BusinessChat(
-        title = R.string.kSuiteBusinessKChatLabel,
-        icon = R.drawable.ic_speech_bubble,
-    ),
-
-    EnterpriseChat(
-        title = R.string.kSuiteEnterpriseKChatLabel,
-        icon = R.drawable.ic_speech_bubble,
-    ),
-
-    StandardMail(
-        title = R.string.kSuiteStandardMailLabel,
-        icon = R.drawable.ic_mail,
-    ),
-
-    BusinessDrive(
+    DriveBusiness(
         title = R.string.kSuiteBusinessKDriveLabel,
         icon = R.drawable.ic_folder,
     ),
 
-    EnterpriseFunctionality(
-        title = R.string.kSuiteEnterpriseFunctionalityLabel,
-        icon = R.drawable.ic_stairs_arrow,
+    DriveEnterprise(
+        title = R.string.kSuiteEnterpriseKDriveLabel,
+        icon = R.drawable.ic_folder,
     ),
 
-    StandardEuria(
+    Euria(
         title = R.string.kSuiteStandardEuriaLabel,
         icon = R.drawable.ic_euria,
     ),
 
-    BusinessSecurity(
-        title = R.string.kSuiteBusinessSecurityLabel,
-        icon = R.drawable.ic_lock,
+    Functionalities(
+        title = R.string.kSuiteEnterpriseFunctionalityLabel,
+        icon = R.drawable.ic_stairs_arrow,
     ),
 
-    EnterpriseMicrosoft(
-        title = R.string.kSuiteEnterpriseMicrosoftLabel,
+    Mail(
+        title = R.string.kSuiteStandardMailLabel,
+        icon = R.drawable.ic_mail,
+    ),
+
+    Microsoft(
+        title = R.string.kSuiteBusinessMicrosoftLabel,
         icon = R.drawable.ic_microsoft,
     ),
 
     More(
         title = R.string.kSuiteMoreLabel,
         icon = R.drawable.ic_plus_circle,
+    ),
+
+    Security(
+        title = R.string.kSuiteBusinessSecurityLabel,
+        icon = R.drawable.ic_lock,
+    ),
+
+    StorageStandard(
+        title = R.string.kSuiteStandardStorageLabel,
+        icon = R.drawable.ic_cloud_disk,
+    ),
+
+    StorageBusiness(
+        title = R.string.kSuiteBusinessStorageLabel,
+        icon = R.drawable.ic_cloud_disk,
+    ),
+
+    StorageEnterprise(
+        title = R.string.kSuiteEnterpriseStorageLabel,
+        icon = R.drawable.ic_cloud_disk,
+    ),
+
+    ChatStandard(
+        title = R.string.kSuiteStandardKChatLabel,
+        icon = R.drawable.ic_speech_bubble,
+    ),
+
+    ChatBusiness(
+        title = R.string.kSuiteBusinessKChatLabel,
+        icon = R.drawable.ic_speech_bubble,
+    ),
+
+    ChatEnterprise(
+        title = R.string.kSuiteEnterpriseKChatLabel,
+        icon = R.drawable.ic_speech_bubble,
     ),
 }
