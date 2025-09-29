0dc5af1e4f266df11ac361bec67a160efffa4965
Jamy Bailly <jamy.bailly@infomaniak.com>
2025-09-29 14:39:52 +0200

    feat: Share BottomStickyButtonScaffold

---
+++ Compose/BottomStickyButtonScaffold/src/main/kotlin/com/infomaniak/core/compose/bottomstickybuttonscaffold/BottomStickyButtonScaffold.kt
@@ -0,0 +1,122 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2024-2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.compose.bottomstickybuttonscaffold
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.infomaniak.core.compose.basicbutton.BasicButton
+import com.infomaniak.core.compose.margin.Margin
+import com.infomaniak.core.compose.preview.PreviewLargeWindow
+import com.infomaniak.core.compose.preview.PreviewSmallWindow
+
+private val DEFAULT_SINGLE_PANE_MAX_WIDTH = 800.dp
+
+@Composable
+fun BottomStickyButtonScaffold(
+    modifier: Modifier = Modifier,
+    snackbarHostState: SnackbarHostState? = null,
+    maxScreenWidth: Dp = DEFAULT_SINGLE_PANE_MAX_WIDTH,
+    buttonComboVerticalPadding: Dp = Margin.Small,
+    topBar: @Composable () -> Unit,
+    topButton: @Composable ((Modifier) -> Unit)? = null,
+    bottomButton: @Composable ((Modifier) -> Unit)? = null,
+    content: @Composable BoxScope.() -> Unit,
+) {
+    SinglePaneScaffold(
+        modifier = modifier,
+        maxScreenWidth = maxScreenWidth,
+        snackbarHost = { snackbarHostState?.let { SnackbarHost(hostState = it) } },
+        topBar = topBar,
+    ) { contentPaddings ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(contentPaddings),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Box(modifier = Modifier.weight(1.0f), content = content)
+            DoubleButtonCombo(
+                modifier = Modifier.padding(vertical = buttonComboVerticalPadding),
+                doubleButtonMaxWidth = maxScreenWidth,
+                topButton = topButton,
+                bottomButton = bottomButton,
+            )
+        }
+    }
+}
+
+@PreviewSmallWindow
+@PreviewLargeWindow
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun Preview() {
+    MaterialTheme {
+        Surface {
+            BottomStickyButtonScaffold(
+                maxScreenWidth = 800.dp,
+                buttonComboVerticalPadding = Margin.Small,
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        colors = TopAppBarDefaults.topAppBarColors(),
+                        title = {
+                            Text(text = "Title")
+                        }
+                    )
+                },
+                topButton = { modifier ->
+                    BasicButton(modifier = modifier, onClick = {}) {
+                        Text("Top Button")
+                    }
+                },
+                bottomButton = { modifier ->
+                    BasicButton(modifier = modifier, onClick = {}) {
+                        Text("Bottom Button")
+                    }
+                },
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.LightGray),
+                    text = "content",
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+    }
+}
---
+++ Compose/BottomStickyButtonScaffold/src/main/kotlin/com/infomaniak/core/compose/bottomstickybuttonscaffold/SinglePaneScaffold.kt
@@ -0,0 +1,79 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.compose.bottomstickybuttonscaffold
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.infomaniak.core.compose.preview.PreviewLargeWindow
+import com.infomaniak.core.compose.preview.PreviewLightAndDark
+
+@Composable
+fun SinglePaneScaffold(
+    modifier: Modifier = Modifier,
+    maxScreenWidth: Dp = 800.dp,
+    topBar: @Composable () -> Unit = {},
+    bottomBar: @Composable () -> Unit = {},
+    snackbarHost: @Composable () -> Unit = {},
+    content: @Composable (PaddingValues) -> Unit,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = topBar,
+        bottomBar = bottomBar,
+        snackbarHost = snackbarHost,
+    ) { contentPadding ->
+        Box(
+            contentAlignment = Alignment.TopCenter,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Box(Modifier.widthIn(max = maxScreenWidth)) {
+                content(contentPadding)
+            }
+        }
+    }
+}
+
+@PreviewLightAndDark
+@PreviewLargeWindow
+@Composable
+private fun Preview() {
+    MaterialTheme {
+        Surface {
+            SinglePaneScaffold {
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .background(Color.Cyan)
+                )
+            }
+        }
+    }
+}
