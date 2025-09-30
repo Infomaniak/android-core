84ea121bb73d363141fdcee34ca7210a86ddb1af
Jamy Bailly <jamy.bailly@infomaniak.com>
2025-09-30 11:01:12 +0200

    fix: Add plurial to module name

--- Compose/BottomStickyButtonScaffolds/src/main/kotlin/com/infomaniak/core/compose/bottomstickybuttonscaffolds/BottomStickyButtonScaffold.kt
+++ Compose/BottomStickyButtonScaffolds/src/main/kotlin/com/infomaniak/core/compose/bottomstickybuttonscaffolds/BottomStickyButtonScaffold.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.infomaniak.core.compose.bottomstickybuttonscaffold
+package com.infomaniak.core.compose.bottomstickybuttonscaffolds
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
