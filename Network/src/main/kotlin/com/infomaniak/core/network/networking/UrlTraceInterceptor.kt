dd9aaa92d9637b4f3d562d381b4fefb56076d017
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2025-09-30 11:57:36 +0200

    feat: Add UrlTraceInterceptor to add the API requestId for each request

---
+++ Network/src/main/kotlin/com/infomaniak/core/network/networking/UrlTraceInterceptor.kt
@@ -0,0 +1,38 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.network.networking
+
+import io.sentry.Breadcrumb
+import io.sentry.Sentry
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class UrlTraceInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val response = chain.proceed(request)
+
+        response.header("x-request-id")?.let { requestId ->
+            val httpBreadcrumb = Breadcrumb.http(request.url.toString(), requestId, response.code)
+            Sentry.addBreadcrumb(httpBreadcrumb)
+        }
+
+        return response
+    }
+}
