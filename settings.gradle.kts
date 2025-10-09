cb10d709126ad328b50d8ec8a1f7811a786b6216
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2025-10-09 09:06:08 +0200

    feat: Start composite build for Onboarding

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("core") {
+            from(files("gradle/core.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "CoreInf"
+
+include(":Compose:Margin")
+include(":Onboarding")
