d54fcaa679691408e58a650376ac8b64692e5b41
Vincent TE <vincent.cheun.ang.te@gmail.com>
2025-10-24 13:53:16 +0200

    chore: Add callback to have the webview

--- WebView/src/main/kotlin/com/infomaniak/core/webview/ui/components/WebView.kt
+++ WebView/src/main/kotlin/com/infomaniak/core/webview/ui/components/WebView.kt
@@ -46,6 +46,7 @@ fun WebView(
     systemBarsColor: Color = Color.Transparent,
     webViewClient: WebViewClient = CustomWebViewClient(urlToQuit, onUrlToQuitReached),
     webChromeClient: WebChromeClient? = null,
+    callback: ((WebView) -> Unit)? = null,
 ) {
     AndroidView(
         modifier = modifier,
@@ -61,6 +62,8 @@ fun WebView(
 
                     settings.javaScriptEnabled = true
                     settings.domStorageEnabled = domStorageEnabled
+
+                    callback?.invoke(this)
 
                     val headers = headersString?.let { Json.decodeFromString<Map<String, String>>(it) } ?: mapOf()
                 }
