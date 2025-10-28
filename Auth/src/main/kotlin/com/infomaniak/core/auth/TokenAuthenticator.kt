06a25cf6191bfc757075a708f1f347d37bc9cc39
Vincent TE <vincent.cheun.ang.te@gmail.com>
2025-10-28 14:29:39 +0100

    chore: Pass accessToken directly instead of ApiToken

--- Auth/src/main/kotlin/com/infomaniak/core/auth/TokenAuthenticator.kt
+++ Auth/src/main/kotlin/com/infomaniak/core/auth/TokenAuthenticator.kt
@@ -19,7 +19,6 @@ package com.infomaniak.core.auth
 
 import com.infomaniak.core.auth.api.AuthRepository
 import com.infomaniak.core.auth.utils.ApiTokenExt.isInfinite
-import com.infomaniak.lib.login.ApiToken
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
@@ -50,10 +49,10 @@ class TokenAuthenticator(
                         tokenInterceptorListener.onRefreshTokenError()
                         null
                     }
-                    isAlreadyRefreshed -> changeAccessToken(request, apiToken)
+                    isAlreadyRefreshed -> changeAccessToken(request, apiToken.accessToken)
                     else -> {
                         val newToken = AuthRepository.refreshToken(apiToken.refreshToken!!, tokenInterceptorListener)
-                        changeAccessToken(request, newToken)
+                        changeAccessToken(request, newToken.accessToken)
                     }
                 }
             }
@@ -63,9 +62,9 @@ class TokenAuthenticator(
     companion object {
         val mutex = Mutex()
 
-        fun changeAccessToken(request: Request, apiToken: ApiToken): Request {
+        fun changeAccessToken(request: Request, accessToken: String): Request {
             val builder = request.newBuilder()
-            builder.header("Authorization", "Bearer ${apiToken.accessToken}")
+            builder.header("Authorization", "Bearer $accessToken")
             return builder.build()
         }
     }
