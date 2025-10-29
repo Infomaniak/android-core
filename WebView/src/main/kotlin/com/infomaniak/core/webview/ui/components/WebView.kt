c534609052cfb68c9726205470b325efc5fcc683
Vincent Te <vincent.te@infomaniak.com>
2025-10-29 13:12:49 +0100

    chore: Add callback for back handler

--- WebView/src/main/kotlin/com/infomaniak/core/webview/ui/components/WebView.kt
+++ WebView/src/main/kotlin/com/infomaniak/core/webview/ui/components/WebView.kt
@@ -24,8 +24,13 @@ import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import kotlinx.serialization.json.Json
@@ -42,12 +47,18 @@ fun WebView(
     webViewClient: WebViewClient = CustomWebViewClient(urlToQuit, onUrlToQuitReached),
     withSafeArea: Boolean = true,
     webChromeClient: WebChromeClient? = null,
+    backHandler: ((WebView) -> Unit)? = null,
     callback: ((WebView) -> Unit)? = null,
 ) {
+    var webView: WebView? by remember { mutableStateOf(null) }
+
+    BackHandler { webView?.let { backHandler?.invoke(it) } }
+
     AndroidView(
         modifier = if (withSafeArea) Modifier.safeDrawingPadding() else Modifier,
         factory = { context ->
             WebView(context).apply {
+                webView = this
                 layoutParams = ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT
