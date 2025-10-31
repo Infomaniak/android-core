fc8491ba55dd29afcb13dc9492a37087e9b5d22e
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2025-10-31 11:03:20 +0100

    chore: Remove useless code

--- WebView/src/main/kotlin/com/infomaniak/core/webview/ui/components/WebView.kt
+++ WebView/src/main/kotlin/com/infomaniak/core/webview/ui/components/WebView.kt
@@ -24,13 +24,8 @@ import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 
@@ -48,13 +43,8 @@ fun WebView(
     webViewClient: WebViewClient = CustomWebViewClient(urlToQuit, onUrlToQuitReached),
     withSafeArea: Boolean = true,
     webChromeClient: WebChromeClient? = null,
-    backHandler: ((WebView) -> Unit)? = null,
     callback: ((WebView) -> Unit)? = null,
 ) {
-    var webView: WebView? by remember { mutableStateOf(null) }
-
-    BackHandler { webView?.let { backHandler?.invoke(it) } }
-
     AndroidView(
         modifier = modifier,
         factory = {
