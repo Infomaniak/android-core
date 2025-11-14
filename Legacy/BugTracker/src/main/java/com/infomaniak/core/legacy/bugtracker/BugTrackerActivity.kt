d3814fb5426521ed43d9982a69b691bc202ae5b4
Jamy Bailly <jamy.bailly@infomaniak.com>
2025-11-14 09:29:19 +0100

    feat: Use AppVersionChecker instead of legacy GithubTool on BugTracker

--- Legacy/BugTracker/src/main/java/com/infomaniak/core/legacy/bugtracker/BugTrackerActivity.kt
+++ Legacy/BugTracker/src/main/java/com/infomaniak/core/legacy/bugtracker/BugTrackerActivity.kt
@@ -136,6 +136,10 @@ class BugTrackerActivity : AppCompatActivity() {
                 appNotUpToDate.isVisible = updateIsAvailable
             }
         }
+
+        // gitHubViewModel.getLastRelease(navigationArgs.repoGitHub).observe(this@BugTrackerActivity) { lastRelease ->
+        //     appNotUpToDate.isGone = lastRelease?.name == navigationArgs.appBuildNumber
+        // }
     }
 
     private fun ActivityBugTrackerBinding.observeBugReportResult() {
