a626b6fa2fc7fe58c5bea5fc3ed5662e90a26198
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2025-11-17 15:37:38 +0100

    refactor: Remove network init from AuthConfig init

--- Auth/src/main/kotlin/com/infomaniak/core/auth/AuthConfiguration.kt
+++ Auth/src/main/kotlin/com/infomaniak/core/auth/AuthConfiguration.kt
@@ -18,8 +18,6 @@
 package com.infomaniak.core.auth
 
 import com.infomaniak.core.auth.networking.AuthHttpClientProvider
-import com.infomaniak.core.network.ApiEnvironment
-import com.infomaniak.core.network.NetworkConfiguration
 import com.infomaniak.core.login.InfomaniakLogin.AccessType
 
 /**
@@ -35,11 +33,9 @@ object AuthConfiguration {
         clientId: String,
         accessType: AccessType? = this.accessType,
         tokenInterceptorListener: TokenInterceptorListener,
-        apiEnvironment: ApiEnvironment = ApiEnvironment.Prod,
     ) {
         this.clientId = clientId
         this.accessType = accessType
         AuthHttpClientProvider.tokenInterceptorListener = tokenInterceptorListener
-        NetworkConfiguration.init(appId, appVersionCode, appVersionName, apiEnvironment)
     }
 }
