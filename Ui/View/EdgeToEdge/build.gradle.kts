0433b6cc20bb1b7134f075c6b31ac7a9ae65de6b
Jamy Bailly <jamy.bailly@infomaniak.com>
2025-11-19 10:02:46 +0100

    fix: Namespace of edgetoedge module

--- Ui/View/EdgeToEdge/build.gradle.kts
+++ Ui/View/EdgeToEdge/build.gradle.kts
@@ -26,7 +26,7 @@ val coreMinSdk: Int by rootProject.extra
 val javaVersion: JavaVersion by rootProject.extra
 
 android {
-    namespace = "com.infomaniak.core.view.edgetoedge"
+    namespace = "com.infomaniak.core.ui.view.edgetoedge"
     compileSdk = coreCompileSdk
 
     defaultConfig {
