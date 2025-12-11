588cd49d49b6e01da0a529a3c03151ee23632257
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2025-12-11 15:23:39 +0100

    chore: Add CompositeBuildConfig args extension for settings.gradle

---
+++ build-logic/composite/src/main/kotlin/com/infomaniak/core/composite/CompositeConfigArgsExtension.kt
@@ -0,0 +1,33 @@
+/*
+ * Infomaniak Core
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.composite
+
+import org.gradle.api.Action
+import org.gradle.api.initialization.Settings
+import org.gradle.api.plugins.ExtensionAware
+
+open class CompositeConfigArgsExtension {
+    var coreRootPath = "Core"
+
+    companion object Companion {
+        internal const val EXTENSION_NAME: String = "CompositeConfigArgsExtension"
+
+        fun Settings.coreCompositeConfig(configure: Action<CompositeConfigArgsExtension>): Unit =
+            (this as ExtensionAware).extensions.configure(EXTENSION_NAME, configure)
+    }
+}
