8a5432e59353b02b8510c69e2a2334596df29435
Vincent Te <vincent.te@infomaniak.com>
2025-12-16 12:36:42 +0100

    chore: Create upsert method for User

--- Auth/src/main/kotlin/com/infomaniak/core/auth/room/UserDao.kt
+++ Auth/src/main/kotlin/com/infomaniak/core/auth/room/UserDao.kt
@@ -21,6 +21,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import androidx.room.Upsert
@@ -65,6 +66,9 @@ interface UserDao {
 
     @Query("SELECT * FROM user WHERE firstname LIKE (:firstName) AND lastname LIKE (:lastName) LIMIT 1")
     suspend fun findByName(firstName: String, lastName: String): User?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(user: User)
 
     @Insert
     suspend fun insert(user: User)
