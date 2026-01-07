85310a2f01a9b2848016239025c8cc7ed587d120
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-01-07 09:52:50 +0100

    feat: Move EndIconTextInputLayout from legacy

--- Ui/View/src/main/kotlin/com/infomaniak/core/ui/view/extension/ViewExt.kt
+++ Ui/View/src/main/kotlin/com/infomaniak/core/ui/view/extension/ViewExt.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.infomaniak.core.ui.view
+package com.infomaniak.core.ui.view.extension
 
 import android.view.View
 import android.view.ViewGroup
