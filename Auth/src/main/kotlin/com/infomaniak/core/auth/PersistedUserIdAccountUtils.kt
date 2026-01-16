ecf5074e6551ebdb93e64f80c49cbff32adacf8a
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-01-16 17:01:00 +0100

    feat: Start adding RoomAccountUtils factorization

---
+++ Auth/src/main/kotlin/com/infomaniak/core/auth/PersistedUserIdAccountUtils.kt
@@ -0,0 +1,44 @@
+/*
+ * Infomaniak SwissTransfer - Android
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.auth
+
+import android.content.Context
+import com.infomaniak.core.auth.models.CurrentUserId
+import com.infomaniak.core.auth.models.user.User
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+
+/**
+ * A version of [AccountUtilsCommon] that automatically stores the current user id inside of room as well
+ */
+abstract class PersistedUserIdAccountUtils(context: Context) : AccountUtilsCommon(context) {
+    override val currentUserIdFlow: Flow<Int?> = userDatabase.currentUserIdDao().getCurrentUserId()
+
+    override suspend fun addUser(user: User) {
+        super.addUser(user)
+        userDatabase.currentUserIdDao().setCurrentUserId(CurrentUserId(user.id))
+    }
+
+    override suspend fun removeUser(user: User) {
+        super.removeUser(user)
+        val currentUserIdDao = userDatabase.currentUserIdDao()
+        if (currentUserIdDao.getCurrentUserId().first() == user.id) {
+            currentUserIdDao.clearCurrentUserId()
+        }
+    }
+}
