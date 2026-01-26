1368663b06bdcef4567acfd1869b1b4831a3fccc
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-01-26 17:06:09 +0100

    feat: Add some more tests

--- Auth/src/test/java/com/infomaniak/core/auth/PersistedUserIdAccountUtilsTest.kt
+++ Auth/src/test/java/com/infomaniak/core/auth/PersistedUserIdAccountUtilsTest.kt
@@ -44,6 +44,66 @@ class PersistedUserIdAccountUtilsTest {
         }
     }
 
+    @Test
+    fun removeUser_unselectsTheCurrentAccount() = runTest {
+        withAccountUtils {
+            addUser(userOf(id = 1))
+            Assert.assertEquals(1, currentUserIdFlow.first())
+
+            removeUser(1)
+            Assert.assertEquals(null, currentUserIdFlow.first())
+        }
+    }
+
+    @Test
+    fun removeUserAndSwitchToNext_selectsTheNextUserCorrectly() = runTest {
+        withAccountUtils {
+            addUser(userOf(id = 1))
+            addUser(userOf(id = 2))
+            removeUserAndSwitchToNext(2)
+            Assert.assertEquals(1, currentUserIdFlow.first())
+        }
+    }
+
+    @Test
+    fun removeUserAndSwitchToNext_unselectsCompletelyWhenRemovingLastUser() = runTest {
+        withAccountUtils {
+            addUser(userOf(id = 1))
+            removeUserAndSwitchToNext(1)
+            Assert.assertEquals(null, currentUserIdFlow.first())
+        }
+    }
+
+    @Test
+    fun switchUser_correctlySelectsExistingAccounts() = runTest {
+        withAccountUtils {
+            addUser(userOf(id = 1))
+            addUser(userOf(id = 2))
+            switchUser(1)
+            Assert.assertEquals(1, currentUserIdFlow.first())
+        }
+    }
+
+    @Test
+    fun switchUser_doesNothingWhenSelectingAccountsThatDontExist() = runTest {
+        withAccountUtils {
+            addUser(userOf(id = 1))
+            addUser(userOf(id = 2))
+            switchUser(3)
+            Assert.assertEquals(2, currentUserIdFlow.first())
+        }
+    }
+
+    @Test
+    fun currentUserIdFlow_canBeReadMultipleTimesInARow() = runTest {
+        withAccountUtils {
+            addUser(userOf(id = 1))
+
+            Assert.assertEquals(1, currentUserIdFlow.first())
+            Assert.assertEquals(1, currentUserIdFlow.first())
+        }
+    }
+
     private inline fun withAccountUtils(block: PersistedUserIdAccountUtils.() -> Unit) {
         val persistedUserIdAccountUtils = object : PersistedUserIdAccountUtils(context, inMemory = true) {}
         block(persistedUserIdAccountUtils)
