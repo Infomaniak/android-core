cad58d1f08d0d913ddbe64657000b13f37dc89b5
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-01-29 16:28:50 +0100

    feat: Add auto-size text support to Avatar component
    Replace `Text` with `BasicText` and apply `TextAutoSize.StepBased` to automatically shrink initials when space is limited. I used the a `Box` to ensure consistent padding across sizes.

--- Avatar/src/main/java/com/infomaniak/core/avatar/components/InitialsAvatar.kt
+++ Avatar/src/main/java/com/infomaniak/core/avatar/components/InitialsAvatar.kt
@@ -19,10 +19,14 @@ package com.infomaniak.core.avatar.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.TextAutoSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -31,9 +35,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.ColorProducer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.infomaniak.core.avatar.models.AvatarColors
 import com.infomaniak.core.avatar.models.AvatarType
 
@@ -46,22 +51,63 @@ internal fun InitialsAvatar(avatarType: AvatarType.WithInitials) {
             .background(avatarType.colors.containerColor),
         contentAlignment = Alignment.Center,
     ) {
-        Text(
-            modifier = Modifier.wrapContentSize(align = Alignment.Center),
-            text = avatarType.initials,
-            color = avatarType.colors.contentColor,
-            textAlign = TextAlign.Center,
-        )
+        Box(
+            modifier = Modifier.fillMaxSize(0.66f),
+            contentAlignment = Alignment.Center,
+        ) {
+            BasicText(
+                modifier = Modifier
+                    .wrapContentSize(align = Alignment.Center),
+                text = avatarType.initials,
+                color = ColorProducer { avatarType.colors.contentColor },
+                autoSize = TextAutoSize.StepBased(8.sp, stepSize = 2.sp),
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 private fun Preview() {
+    @Composable
+    fun Section(text: String) {
+        Text(text, fontSize = 10.sp, lineHeight = 10.sp, modifier = Modifier.padding(top = 8.dp))
+    }
+
     MaterialTheme {
         Surface {
-            Box(modifier = Modifier.size(32.dp)) {
-                InitialsAvatar(AvatarType.WithInitials.Initials("IK", AvatarColors(Color(0xFF3CB572), Color.White)))
+            Column {
+                Section("24.dp")
+                Box(modifier = Modifier.size(24.dp)) {
+                    InitialsAvatar(AvatarType.WithInitials.Initials("WW", AvatarColors(Color(0xFF3CB572), Color.White)))
+                }
+                Box(modifier = Modifier.size(24.dp)) {
+                    InitialsAvatar(AvatarType.WithInitials.Initials("II", AvatarColors(Color(0xFF3CB572), Color.White)))
+                }
+
+                Section("32.dp")
+                Box(modifier = Modifier.size(32.dp)) {
+                    InitialsAvatar(AvatarType.WithInitials.Initials("WW", AvatarColors(Color(0xFF3CB572), Color.White)))
+                }
+                Box(modifier = Modifier.size(32.dp)) {
+                    InitialsAvatar(AvatarType.WithInitials.Initials("II", AvatarColors(Color(0xFF3CB572), Color.White)))
+                }
+
+                Section("40.dp")
+                Box(modifier = Modifier.size(40.dp)) {
+                    InitialsAvatar(AvatarType.WithInitials.Initials("WW", AvatarColors(Color(0xFF3CB572), Color.White)))
+                }
+                Box(modifier = Modifier.size(40.dp)) {
+                    InitialsAvatar(AvatarType.WithInitials.Initials("II", AvatarColors(Color(0xFF3CB572), Color.White)))
+                }
+
+                Section("80.dp")
+                Box(modifier = Modifier.size(80.dp)) {
+                    InitialsAvatar(AvatarType.WithInitials.Initials("WW", AvatarColors(Color(0xFF3CB572), Color.White)))
+                }
+                Box(modifier = Modifier.size(80.dp)) {
+                    InitialsAvatar(AvatarType.WithInitials.Initials("II", AvatarColors(Color(0xFF3CB572), Color.White)))
+                }
             }
         }
     }
