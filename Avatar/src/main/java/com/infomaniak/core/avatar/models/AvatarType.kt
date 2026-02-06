912e39aa24f17e194a5e88bac1d7b6af9535d8ad
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-02-06 10:17:15 +0100

    docs: Add kdoc around AvataryType.fromUser()

--- Avatar/src/main/java/com/infomaniak/core/avatar/models/AvatarType.kt
+++ Avatar/src/main/java/com/infomaniak/core/avatar/models/AvatarType.kt
@@ -19,7 +19,6 @@ package com.infomaniak.core.avatar.models
 
 import androidx.annotation.DrawableRes
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
@@ -62,17 +61,19 @@ sealed interface AvatarType {
             }
         }
 
+        /**
+         * Automatically returns the appropriate AvatarType to display the provided [user]. The style in which the avatar is going
+         * to be displayed depends on the theme provided through [LocalAvatarColors].
+         */
         @Composable
         fun fromUser(user: User): WithInitials {
             val context = LocalContext.current
             val avatarColors = LocalAvatarColors.current
 
             return getUrlOrInitials(
-                // TODO: See if we want to customize the image loader
                 avatarUrlData = user.avatar?.let { AvatarUrlData(it, SingletonImageLoader.get(context)) },
                 initials = user.getInitials(),
                 colors = AvatarColors(
-                    // TODO: See if it works with custom dark mode local to the app
                     containerColor = getBackgroundColorResBasedOnId(user.id, avatarColors.containerColors),
                     contentColor = avatarColors.contentColor,
                 ),
