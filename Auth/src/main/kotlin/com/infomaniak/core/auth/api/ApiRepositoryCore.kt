0419a71e7cca565b921bb9f81961e1eac1f8e164
Vincent Te <149579879+tevincent@users.noreply.github.com>
2026-04-21 13:48:46 +0200

    chore: Update project name in ApiRepositoryCore.kt

--- Auth/src/main/kotlin/com/infomaniak/core/auth/api/ApiRepositoryCore.kt
+++ Auth/src/main/kotlin/com/infomaniak/core/auth/api/ApiRepositoryCore.kt
@@ -1,5 +1,5 @@
 /*
- * Infomaniak Authenticator - Android
+ * Infomaniak Core - Android
  * Copyright (C) 2022-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
