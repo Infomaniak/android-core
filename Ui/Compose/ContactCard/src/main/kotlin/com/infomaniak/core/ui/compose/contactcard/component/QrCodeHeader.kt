2fd09f3a3ce4d70cafc6253037a836b599149b10
Elouan BOITEUX <elouan.boiteux@infomaniak.com>
2026-07-16 17:54:34 +0200

    fix: Add white background behind QR code

--- Ui/Compose/ContactCard/src/main/kotlin/com/infomaniak/core/ui/compose/contactcard/component/QrCodeHeader.kt
+++ Ui/Compose/ContactCard/src/main/kotlin/com/infomaniak/core/ui/compose/contactcard/component/QrCodeHeader.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -71,7 +72,7 @@ internal fun QrCodeHeader(user: User, card: Card) {
         Surface(
             shape = RoundedCornerShape(CardCornerRadius),
             modifier = Modifier.padding(top = gradientHeight * 0.5f),
-            color = MaterialTheme.colorScheme.surface,
+            color = Color.White,
             shadowElevation = 2.dp,
         ) {
             Box(
