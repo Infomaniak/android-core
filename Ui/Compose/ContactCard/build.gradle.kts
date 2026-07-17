83b73203400fbcc4781d9bc7c7b744a96ec8bafc
Elouan BOITEUX <elouan.boiteux@infomaniak.com>
2026-07-17 15:01:25 +0200

    refactor: Remove unused dependencies

--- Ui/Compose/ContactCard/build.gradle.kts
+++ Ui/Compose/ContactCard/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(core.plugins.infomaniak.android.library)
     alias(core.plugins.infomaniak.android.library.flavor.aware)
@@ -16,18 +14,16 @@ android {
 }
 
 dependencies {
+    api(project(":Network"))
     implementation(project(":Auth"))
-    implementation(project(":Network"))
     implementation(project(":Avatar"))
     implementation(project(":Common"))
     implementation(project(":Ui:View:PrimaryPalette"))
     implementation(project(":Ui:Compose:Margin"))
-    implementation(project(":Ui:Compose:MaterialThemeFromXml"))
 
     implementation(platform(core.compose.bom))
     api(core.compose.runtime)
     api(core.compose.ui)
-    implementation(core.compose.ui.android)
     implementation(core.compose.foundation)
     implementation(core.compose.material3)
     implementation(core.compose.material.icons)
@@ -37,7 +33,5 @@ dependencies {
     implementation(core.androidx.lifecycle.viewmodel.ktx)
     implementation(core.androidx.lifecycle.viewmodel.compose)
     implementation(core.androidx.lifecycle.runtime.compose)
-    implementation(core.navigation.fragment.ktx)
-    implementation(core.activity.compose)
     implementation(core.qrose)
 }
