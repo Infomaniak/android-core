5549e92b58dc8ef99dc22ace24989a8a39c61b0b
Elouan BOITEUX <elouan.boiteux@infomaniak.com>
2026-07-23 18:15:13 +0200

    feat: Add ContactCard top bars

---
+++ Ui/Compose/ContactCard/src/main/kotlin/com/infomaniak/core/ui/compose/contactcard/component/PreviewTopBar.kt
@@ -0,0 +1,70 @@
+/*
+ * Infomaniak Core - Android
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.core.ui.compose.contactcard.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.infomaniak.core.ui.compose.contactcard.R
+import com.infomaniak.core.common.R as RCore
+
+@Composable
+internal fun PreviewTopBar(
+    onClose: () -> Unit,
+    onMore: () -> Unit,
+) {
+    ContactCardTopBar(
+        navigationIcon = {
+            IconButton(onClick = onClose) {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = stringResource(RCore.string.buttonCancel),
+                    tint = MaterialTheme.colorScheme.primary,
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = onMore) {
+                Icon(
+                    imageVector = Icons.Filled.MoreVert,
+                    contentDescription = stringResource(R.string.buttonMore),
+                    tint = MaterialTheme.colorScheme.primary,
+                )
+            }
+        },
+    )
+}
+
+@Preview(name = "PreviewTopBar")
+@Composable
+private fun PreviewTopBarPreview() {
+    MaterialTheme {
+        Surface {
+            PreviewTopBar(
+                onClose = {},
+                onMore = {},
+            )
+        }
+    }
+}
