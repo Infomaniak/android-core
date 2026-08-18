adeab2670ba069cabec44644c3c3ed6e411aac52
Louis CAD <louis.cognault@infomaniak.com>
2026-08-18 10:33:30 +0200

    test: Allow DerivedTokenGenerator to be subclassed from tests

--- Auth/src/main/kotlin/com/infomaniak/core/auth/DerivedTokenGenerator.kt
+++ Auth/src/main/kotlin/com/infomaniak/core/auth/DerivedTokenGenerator.kt
@@ -22,7 +22,7 @@ import com.infomaniak.core.common.Xor
 import com.infomaniak.core.login.ApiToken
 import okhttp3.Response
 
-sealed interface DerivedTokenGenerator {
+interface DerivedTokenGenerator {
 
     suspend fun attemptDerivingOneOfTheseTokens(tokensToTry: Set<String>): Xor<ApiToken, Issue>
     suspend fun isAppIntegrityGuaranteedToFail(): Boolean
