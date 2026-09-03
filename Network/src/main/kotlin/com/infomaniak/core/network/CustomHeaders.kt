210602cc7b8c5ff4a059946fff9f10382842514c
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2026-09-03 14:10:37 +0200

    feat: Add remove function

--- Network/src/main/kotlin/com/infomaniak/core/network/CustomHeaders.kt
+++ Network/src/main/kotlin/com/infomaniak/core/network/CustomHeaders.kt
@@ -30,6 +30,10 @@ class CustomHeaders {
         customHeaders[key] = value
     }
 
+    fun remove(key: String) {
+        customHeaders.remove(key)
+    }
+
     fun clear() {
         customHeaders.clear()
     }
